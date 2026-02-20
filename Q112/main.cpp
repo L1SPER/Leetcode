9545e8051c5867dff2616d0c5647e8e2d9bf2f6f
ALİM ARSLAN <alimarslan003@gmail.com>
2026-02-21 01:24:11 +0300

    Question 112 is solved

---
+++ Q112/main.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+struct TreeNode {
+	int val;
+	TreeNode* left;
+	TreeNode* right;
+	TreeNode() : val(0), left(nullptr), right(nullptr) {}
+	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+	TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+	
+};
+bool hasPathSum(TreeNode* root, int targetSum) {
+	if (!root) return false;
+	
+	if (!root->left && !root->right)
+	{
+		if (root->val-targetSum == 0)
+			return true;
+		else
+			return false;
+	}
+	
+	return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum - root->val);
+}
+int main()
+{
+	TreeNode* root = new TreeNode(5);
+	root->left = new TreeNode(4);
+	root->right = new TreeNode(8);
+	root->left->left = new TreeNode(11);
+	root->left->left->left = new TreeNode(7);
+	root->left->left->right = new TreeNode(2);
+	root->right->left = new TreeNode(13);
+	root->right->right = new TreeNode(4);
+	root->right->right->right = new TreeNode(1);
+	int targetSum = 22;
+	cout << hasPathSum(root, targetSum) << endl;
+
+	return 0;
+}
